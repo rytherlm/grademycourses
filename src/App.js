b2656190cab4005e2cb54be5aa3b135815c37a69
rytherlm <aidanryther15@gmail.com>
2023-06-22 14:47:07 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
-
+import SearchBar from './SearchBar';
+import './App.css';
 
 function App() {
- 
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+
+    // Simulate searching for class names
+    const classFiles = require.context('./pages', true, /\.js$/);
+    const classes = classFiles.keys().map((key) => key.slice(2, -3)); // Extract class names
+
+    const matchingClasses = classes.filter((className) =>
+      className.includes(query)
+    );
+
+    setSearchResults(matchingClasses);
+  };
 
   return (
     <div>
       <Navbar />
-      
+      <div className="search-container">
+        <SearchBar onSearch={handleSearch} />
+      </div>
+      {searchResults.length > 0 && (
+        <div className="search-results">
+          <p>Search Results for: {searchQuery}</p>
+          <ul>
+            {searchResults.map((className) => (
+              <li key={className}>{className}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/SearchBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search for a class"
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
