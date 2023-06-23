86b010f0b458e45c46ab24900dc0201cb3f12c90
rytherlm <aidanryther15@gmail.com>
2023-06-23 08:44:10 -0400

    Working on new pages add getting the auth state able to be accesed in any file

--- src/classes/csci99.js
+++ src/classes/csci99.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import './styles/csci99.css';
+import { AuthContext } from '../AuthContext'; // Update the import path
 
-function csci99() {
+const Csci99 = () => {
+  const { isLoggedIn } = useContext(AuthContext); // Access the isLoggedIn state
+
   return (
     <div>
-      <p>bruh 99</p>
+      <h1>My Page</h1>
+      <p>{isLoggedIn ? 'Logged In' : 'Logged Out'}</p>
+      {/* Render different content based on the login status */}
     </div>
   );
-}
+};
 
-export default csci99;
+export default Csci99;
